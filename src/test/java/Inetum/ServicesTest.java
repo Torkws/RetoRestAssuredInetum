f80b01877b317ecaf91a598ad94c992358494eaf
Torkws <47335736+Torkws@users.noreply.github.com>
2025-12-03 19:32:55 -0500

    Fix: Adecuaciones SerenityBDD en pom.xml

--- src/test/java/Inetum/ServicesTest.java
+++ src/test/java/Inetum/ServicesTest.java
@@ -1,4 +1,16 @@
 package Inetum;
 
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "Inetum.StepDefinitions",
+        plugin = {"pretty", "json:target/build/cucumber.json"},
+        tags = "@step"
+)
+
 public class ServicesTest {
 }
---
+++ src/test/java/Inetum/StepDefinitions/api_Listar_usuariosStepDefinition.java
@@ -0,0 +1,12 @@
+package Inetum.StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class api_Listar_usuariosStepDefinition {
+
+
+
+
+}
